fd56c69aa64b9828c30d9114b9b2365fd4c0da93
Cash Wu <cash@cashwu.com>
2018-12-29 17:39:01 +0800

    04 介紹Stateful Widget及Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: "my flutter app",
-    home: new Center(
-        child: new Scaffold(
-      appBar: new AppBar(
-        title: new Text("App bar"),
+  runApp(new MyApp());
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _MyHomePage();
+  }
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  WordPair wordPair = WordPair.random();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            wordPair.asPascalCase,
+            style: new TextStyle(fontSize: 20, color: Colors.blue),
+          ),
+          new RaisedButton(
+            onPressed: _onPressedFun,
+            child: new Text("Button"),
+          )
+        ],
       ),
-      body: new Center(
-        child: new Text(WordPair.random().asCamelCase,
-            style: new TextStyle(fontSize: 20, color: Colors.blue)),
-      ),
-    )),
-  ));
+    );
+  }
+
+  void _onPressedFun() {
+    setState(() {
+      wordPair = WordPair.random();
+    });
+    print(wordPair);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  WordPair wordPair = WordPair.random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "my flutter app",
+        home: new Scaffold(
+          appBar: new AppBar(title: new Text("app bar")),
+          body: new MyHomePage(),
+        ));
+  }
+
+  void _onPressedFun() {
+    wordPair = WordPair.random();
+    print(wordPair);
+  }
 }
